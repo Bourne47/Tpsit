d9c96b5256841bc1a3c0af007733fc4772d905c4
Leonardo Pollicino <leonardo.pollicino@itiscuneo.eu>
2023-10-09 08:13:21 +0200

    caricato esercizio somma elementi di un array
    L'esercizio somma i numeri interi presenti dentro un array utilizzando l'aritmetica dei puntatori.

---
+++ c/es_2/es_somma_Pollicino.c
@@ -0,0 +1,28 @@
+/*
+author: Pollicino Leonardo
+date: 02/10/2023
+es. 1
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define DIM 10
+
+int calcolaSomma (int vett[], int num){
+    int somma = 0;
+    for (int c = 0; c < num; c++){
+        somma += *(vett + c);
+    }
+    return somma;
+}
+
+int main(){
+    int vett[DIM] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int somma = calcolaSomma(vett, DIM);
+    printf("%d", somma);
+
+    return 0;
+}
